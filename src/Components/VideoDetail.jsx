b5f540e6187b24d57ef0a16fd0347a863284f564
Kartar Jabanda <kartar.jabanda@remarkable.net>
2020-02-21 13:29:15 +0000

    attempt to create video player solo

--- src/Components/VideoDetail.jsx
+++ src/Components/VideoDetail.jsx
@@ -5,7 +5,16 @@ const VideoDetail = ({ video }) => {
         return <div>Loading...</div>
     } 
 
-    return <div>{video.snippet.title}</div>
+    return (
+        <div>
+            <div className="ui segment">
+                <img className="ui image" src={video.snippet.thumbnails.medium.url} alt="thumbnail" />
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+
+        </div>
+    )
 }
 
 export default VideoDetail;
