527c5d46b002455df31297c27621cb1aec132710
Kartar Jabanda <kartar.jabanda@remarkable.net>
2020-02-26 15:05:34 +0000

    start again

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,42 +1,10 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-import VideoList from "./VideoList";
-import VideoDetail from './VideoDetail';
-
-import youtube from "../api/youtube";
+import React from "react";;
 
 class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
-
-  onTermSubmit = async term => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term
-      }
-    }); // waiting for response from YT API
-
-    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
-  };
-
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video });
-  };
 
   render() {
     return (
-      <div className="ui container">
-         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui four column grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>App</div>
     ); // function is in App but we passing it down as props - VideoList
   }
 }
